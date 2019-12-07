f4bc13e0e02080077e9028201336ffe3f3fc1662
Firat Ozpinar <firatozpinar@gmail.com>
2019-12-07 16:29:12 +0300

    turkish language added

---
+++ dist/js/i18n/datepicker.tr.js
@@ -0,0 +1,12 @@
+;(function ($) { $.fn.datepicker.language['tr'] = {
+    days: ['Pazar', 'Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi'],
+    daysShort: ['Pz', 'Pzt', 'Sal', 'Çrş', 'Prş', 'Cu', 'Cts'],
+    daysMin: ['Pz', 'Pzt', 'Sa', 'Çr', 'Pr', 'Cu', 'Ct'],
+    months: ['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran', 'Temmuz', 'Ağustos', 'Eylül', 'Ekim', 'Kasım', 'Aralık'],
+    monthsShort: ['Oca', 'Şub', 'Mar', 'Nis', 'May', 'Haz', 'Tem', 'Ağu', 'Eyl', 'Eki', 'Kas', 'Ara'],
+    today: 'Bugün',
+    clear: 'Temizle',
+    dateFormat: 'dd/mm/yyyy',
+    timeFormat: 'hh:ii aa',
+    firstDay: 0
+}; })(jQuery);
